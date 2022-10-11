3a211a094c0f6a9c846af56b2d116b329cdd7f6c
Michael Stahnke <stahnma@devops.af>
2022-10-10 22:41:50 -0500

    Update fixtures to include more quotes

--- fixtures/quote.sh
+++ fixtures/quote.sh
@@ -6,3 +6,7 @@ curl  -v \
  --data-urlencode  "quote=I know what you did last summer." \
    http://giga2:4567/quote
 
+curl  -v \
+ --data-urlencode  "author=John Lennon" \
+ --data-urlencode  "quote=All you need is love" \
+   http://giga2:4567/quote
