708210232538d76e00d45665caad28fdcbf5e7e6
Michael Stahnke <stahnma@devops.af>
2022-10-11 23:18:41 -0500

    Update link fixtures

--- fixtures/link.sh
+++ fixtures/link.sh
@@ -1,7 +1,21 @@
 #/usr/bin/env bash
 
-# Write a quote
+#curl  -v \
+# --data-urlencode  "user=stahnma" \
+# --data-urlencode  "url=http://www.reddit.com" \
+#   http://giga2:4567/link
+
 curl  -v \
  --data-urlencode  "user=stahnma" \
- --data-urlencode  "url=http://www.reddit.com" \
+ --data-urlencode  "url=https://www.infoq.com/presentations/stripe-dev-env-infrastructure/ "\
    http://giga2:4567/link
+
+
+# It should return the id of the link posted
+# It should find the title
+# If it's a 404, tell the user.
+# If it's a 500 tell the user.
+# It should validate SSL?
+# If it has no title, fall back to url
+# if it's a duplicate URI, return a snarky remark
+
