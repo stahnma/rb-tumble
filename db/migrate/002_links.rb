282da7bed2b7de25ee256d90976f2030eb157626
Michael Stahnke <stahnma@devops.af>
2022-10-13 23:30:39 -0500

    Remove not null requirements in links for title

--- db/migrate/002_links.rb
+++ db/migrate/002_links.rb
@@ -2,8 +2,8 @@ class Links < ActiveRecord::Migration[6.0]
   def change
     create_table :links do |t|
       t.string :url, null: false
-      t.string :user, null: false, default: ''
-      t.string :title, null: false, default: ''
+      t.string :user,  default: ''
+      t.string :title, default: ''
       t.integer :clicks, null: false, default: 0
       t.string :content_type
       t.timestamps
