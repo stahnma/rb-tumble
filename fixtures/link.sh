8251c7f20dbdbb0e0b6ceb20564a492116e62811
Michael Stahnke <stahnma@devops.af>
2023-01-03 02:21:42 +0000

    Change username in fixtures to be test-user
    This is because it's easier to clean out the database if all the
    automated stuff comes from one user.

--- fixtures/link.sh
+++ fixtures/link.sh
@@ -1,50 +1,58 @@
 #/usr/bin/env bash
 
 curl  -v \
- --data-urlencode  "user=stahnma" \
+ --data-urlencode  "user=test-user" \
  --data-urlencode  "url=http://www.reddit.com" \
    $WEBURI/link
 
 curl  -v \
- --data-urlencode  "user=stahnma" \
+ --data-urlencode  "user=test-user" \
  --data-urlencode  "url=http://www.section303.com" \
    $WEBURI/link
 
 curl  -v \
- --data-urlencode  "user=stahnma" \
+ --data-urlencode  "user=test-user" \
  --data-urlencode  "url=https://www.infoq.com/presentations/stripe-dev-env-infrastructure/ "\
    $WEBURI/link
 
 curl  -v \
- --data-urlencode  "user=stahnma" \
+ --data-urlencode  "user=test-user" \
  --data-urlencode  "url=http://twitter.com" \
    $WEBURI/link
 
 curl  -v \
- --data-urlencode  "user=stahnma" \
+ --data-urlencode  "user=test-user" \
  --data-urlencode  "url=http://example.com" \
    $WEBURI/link
 
 curl  -v \
- --data-urlencode  "user=stahnma" \
+ --data-urlencode  "user=test-user" \
  --data-urlencode  "url=http://countingcrows.com" \
    $WEBURI/link
 
 curl -v \
- --data-urlencode  "user=somebody" \
+ --data-urlencode  "user=test-user" \
  --data-urlencode  "url=https://twitter.com/adamhjk/status/1580728510347608064" \
    $WEBURI/link
 
 curl -v \
- --data-urlencode  "user=video dude" \
+ --data-urlencode  "user=test-user" \
  --data-urlencode  "url=https://youtu.be/yslCUtpe_F8" \
    $WEBURI/link
 
 curl -v \
- --data-urlencode  "user=video dude" \
+ --data-urlencode  "user=test-user" \
  --data-urlencode  "url=https://www.youtube.com/watch?v=yslCUtpe_F8" \
  --data-urlencode "channel=#general" \
    $WEBURI/link
+
+
+link=(curl -v \
+ --data-urlencode  "user=test-user" \
+ --data-urlencode  "url=https://www.youtube.com/watch?v=yslCUtpe_F8" \
+ --data-urlencode "channel=#general" \
+ $WEBURI/link)
+echo $link
 
 
 # It should return the id of the link posted
